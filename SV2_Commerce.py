707ba38ad337bca70fa1f846aec15dda4adbccb6
Dave <136987161+DimitriGlazov@users.noreply.github.com>
2024-11-21 17:29:40 +0530

    Update SV2_Commerce.py

--- SV2_Commerce.py
+++ SV2_Commerce.py
@@ -60,7 +60,7 @@ else:
                 student_performance['Subjects'] = student_performance.index
 
                 # Enhanced Bar Chart
-                bar_colors = ['#4e79a7', '#f28e2b', '#f5deb3', '#2ca02c', '#edc948']
+                bar_colors =  ['#1f77b4', '#ff7f0e', '#2ca02c', '#d62728', '#9467bd']
                 fig = px.bar(student_performance, x='Subjects', y='Marks', color='Marks',
                              color_continuous_scale=bar_colors, title=f"{splitname}'s Performance in Selected Subjects")
 
